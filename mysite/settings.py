3c6fb4e3fdb36c527615cc11ac89b576392183ce
ppgallas <ppgallas@gmail.com>
2018-06-12 16:54:47 +0200

    added commenting functionality to the app

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cn@^ofw5^my*^_e-j2t0fn2b^p9x0)$j$&tyvu3m($6vd86a$b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ppgallas.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
