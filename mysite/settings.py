5101e43ec79349437b975e4a6e22b2e606840052
ppgallas <ppgallas@gmail.com>
2018-06-12 12:53:22 +0200

    changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-=======
+
 #url redirections after succesful login
 
 LOGIN_REDIRECT_URL = '/'
