e41183947871689a30a7bce6c4954e0bc4bc1975
ppgallas <ppgallas@gmail.com>
2018-06-26 21:21:03 +0200

    added authorisation to main page

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.decorators import login_required
 
 from django.core.files.storage import FileSystemStorage
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
